fc2ada15511f72e32ae6b986f958ff988acb3a5a
luckystar91121 <95670219+luckystar91121@users.noreply.github.com>
2022-01-14 17:13:56 +0900

    fix admin profile update page

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
+from main.models import Employee
 
 from users.models import *
 from trainers.models import *
@@ -29,31 +30,30 @@ def admin_logout(request):
     return redirect("/login")
 @login_required
 def admin_profile(request):
+    context={}
     if request.method=="POST":
         if request.POST.get("name"):
             request.user.username = request.POST.get("name")
         if request.POST.get("email"):
             request.user.email = request.POST.get("email")
         if request.FILES.get("auth_profile_photo"):
-            request.user.employee.photo = request.FILES.get("auth_profile_photo")
-            
+            if request.user.employee:
+                request.user.employee.photo = request.FILES.get("auth_profile_photo")
+            else:
+                request.user.employee = Employee.objects.create(
+                    photo = request.FILES.get("auth_profile_photo")
+                )
         request.user.save()
-        request.user.employee.save()
-
-        context = {}
-        context["title"] = "Dashboard"
-        context["dashboard_section"] = "current_section"
-        context['user'] = request.user
-        return render(request, 'auth/profile.html', context)
-    else:
-        context={}
-        context["title"] = "Dashboard"
-        context["dashboard_section"] = "current_section"
-        context['user']=request.user
-        return render(request, 'auth/profile.html',context)
+        request.user.employee.save()        
+        
+    context["title"] = "Dashboard"
+    context["dashboard_section"] = "current_section"
+    context['user']=request.user
+    return render(request, 'auth/profile.html',context)
 
 @login_required
 def admin_change(request):
+    context = {}
     if request.method=="POST":
         message = "Password changed successfully"
         if check_password(request.POST.get('old_password'), request.user.password):
@@ -64,19 +64,11 @@ def admin_change(request):
                 message="New password do not match"
         else:
             message = "Old password is incorrect"
-
-        context = {}
-        context["title"] = "Dashboard"
-        context["dashboard_section"] = "current_section"
-        context['user'] = request.user
         context['message'] = message   
-        return render(request, 'auth/change_password.html', context)
-    else:
-        context = {}
-        context["title"] = "Dashboard"
-        context["dashboard_section"] = "current_section"
-        context['user'] = request.user    
-        return render(request, 'auth/change_password.html', context)
+    context["title"] = "Dashboard"
+    context["dashboard_section"] = "current_section"
+    context['user'] = request.user    
+    return render(request, 'auth/change_password.html', context)
 
 def admin_login(request):
     if request.user.is_authenticated:
